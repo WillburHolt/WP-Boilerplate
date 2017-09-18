54aa01c164de23c90edc6c633068408228ecc257
Stephen Sabatini <stephensabatini@ssabatini.local>
2017-09-18 19:50:31 -0400

    Build Listing Page

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+	/**
+	 * Blog Index / Main Template File
+	 *
+	 * Typically used for the blog index, this is also the default template if no
+	 * other templates are provided.
+	 *
+	 * @link https://developer.wordpress.org/themes/template-files-section/post-template-files/
+	 * @author Fastspot
+	 * @package WordPress
+	 * @subpackage Boilerplate
+	 * @since 1.0.0
+	 * @version 1.0.0
+	 */
+
+	// Everything is stored in our namespace.
+	namespace Boilerplate;
+
+	get_header();
+
+	if (have_posts()) {
+		$post_id = get_the_ID();
+		$feature_callouts = get_field('feature', $post_id);
+		$in_content_callouts = get_field('in_content', $post_id);
+		$full_width_callouts = get_field('full_width', $post_id);
+?>
+<!-- Page -->
+<div class="page">
+	<?php if (is_array($feature_callouts) && count($feature_callouts)) { ?>
+	<!-- Page Feature -->
+	<div class="page_feature">
+		<?php Boilerplate::drawCallouts($feature_callouts, 'feature', $post_id); ?>
+	</div>
+	<!-- END: Page Feature -->
+	<?php } ?>
+	<!-- Page Content -->
+	<div class="page_content">
+		<?php if (is_array($in_content_callouts) && count($in_content_callouts)) { ?>
+		<div class="row">
+			<!-- Sub Nav -->
+			<div class="subnav_cell">
+				<?php Boilerplate::drawSubNav(); ?>
+			</div>
+			<!-- END: Sub Nav -->
+			<!-- Main Content -->
+			<div class="content_cell">
+				<main class="main_content" id="main_content" itemprop="mainContentOfPage">
+					<!-- In Content Callouts -->
+					<div class="in_content_callouts">
+						<?php Boilerplate::drawCallouts($in_content_callouts, 'in-content', $post_id); ?>
+					</div>
+					<!-- END: In Content Callouts -->
+				</main>
+			</div>
+			<!-- END: Main Content -->
+			<?php get_sidebar(); ?>
+		</div>
+		<?php } ?>
+		<!-- Full Width Callouts -->
+		<div class="full_width_callouts">
+			<?php
+				Boilerplate::drawBlogListing($full_width_callouts, $post_id);
+				Boilerplate::drawCallouts($full_width_callouts, 'full-width', $post_id);
+			?>
+		</div>
+		<!-- END: Full Width Callouts -->
+	</div>
+	<!-- END: Page Content -->
+</div>
+<!-- END: Page -->
+<?php
+	}
+	get_footer();
+?>
