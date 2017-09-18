f8a86d72db8b655f7e1cc72ab08596bd27d18b91
Stephen Sabatini <stephensabatini@ssabatini.local>
2017-09-18 19:49:21 -0400

    Build Map Component

---
+++ template-parts/components/feature/map.php
@@ -0,0 +1,25 @@
+<?php
+	/**
+	 * Map Component
+	 *
+	 * @author Fastspot
+	 * @package WordPress
+	 * @subpackage Boilerplate
+	 * @since 1.0.0
+	 * @version 1.0.0
+	 */
+
+	// Everything is stored in our namespace.
+	namespace Boilerplate;
+?>
+<section class="map">
+	<div class="map_aside">
+		<div class="map_groups"></div>
+	</div>
+	<div class="map_body">
+		<div class="map_filters"></div>
+		<div class="map_canvas" id="map"></div>
+		<div class="map_lightbox"></div>
+		<div class="map_layer_switches"></div>
+	</div>
+</section>
