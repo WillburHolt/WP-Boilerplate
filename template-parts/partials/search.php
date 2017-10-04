32fee88f2994ebe76acdcda5b0220684978ae753
Stephen Sabatini <ssabatini@fastspot.com>
2017-10-03 21:59:37 -0400

    Correct URL.

--- template-parts/partials/search.php
+++ template-parts/partials/search.php
@@ -17,8 +17,8 @@
 	$search_link = isset($_REQUEST) ? add_query_arg($_REQUEST, get_search_link()) : get_search_link();
 ?>
 <div class="site_search<?php if ($modifier) echo " site_search_$modifier"; ?>" id="site_search<?php if ($modifier) echo "_$modifier"; ?>" itemscope itemtype="http://schema.org/WebSite" role="search">
-	<meta itemprop="url" content="<?php Boilerplate::drawDomain(); ?>">
-	<form class="site_search_form" itemprop="potentialAction" itemscope itemtype="http://schema.org/SearchAction">
+	<meta itemprop="url" content="<?php the_permalink(); ?>">
+	<form class="site_search_form" itemprop="potentialAction" itemscope itemtype="http://schema.org/SearchAction" action="/search/">
 		<meta itemprop="target" content="<?=$search_link?>">
 		<label class="site_search_label" for="search_term_string<?php if ($modifier) echo "_$modifier"; ?>"><?php _e('Search'); ?></label>
 		<input aria-live="polite" class="site_search_input" itemprop="query-input" type="text" id="search_term_string<?php if ($modifier) echo "_$modifier"; ?>" name="search_term_string<?php if ($modifier) echo "_$modifier"; ?>" placeholder="<?=$placeholder?>">
