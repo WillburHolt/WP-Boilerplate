db21e850b2555d4e30536c4d13849fdd84685047
Stephen Sabatini <ssabatini@fastspot.com>
2017-11-20 10:38:54 -0500

    Add title to search page.

--- search.php
+++ search.php
@@ -17,11 +17,23 @@
 
 	while (have_posts()) {
 		the_post();
+		$title = get_field('search_page_title', 'option');
 ?>
 <!-- Page -->
 <div class="page">
 	<!-- Page Content -->
 	<div class="page_content">
+		<!-- Main Content -->
+		<div class="fs-row">
+			<div class="fs-cell fs-lg-10 fs-centered content_cell">
+				<main class="main_content" id="main_content" itemprop="mainContentOfPage">
+					<div class="page_header">
+						<h1 class="page_title" id="page_title"><?=esc_html($title)?></h1>
+					</div>
+				</main>
+			</div>
+		</div>
+		<!-- END: Main Content -->
 		<!-- Full Width Callouts -->
 		<div class="full_width_callouts">
 			<?php
